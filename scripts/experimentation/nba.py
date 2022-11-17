78a28a13bc897c4d8a3cc3c18ebc805ed4106272
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2022-11-17 10:31:47 +0100

    Dayli update

---
+++ scripts/experimentation/nba.py
@@ -0,0 +1,48 @@
+import json
+
+from nba_api.live.nba.endpoints import scoreboard
+from nba_api.stats.static import teams
+from nba_api.stats.endpoints import commonteamroster, scoreboardv2
+import pandas
+from nba_api.live.nba.endpoints import playbyplay
+from nba_api.live.nba.endpoints import boxscore
+
+from nba_api.stats.endpoints import leagueseasonmatchups
+
+
+# Today's Score Board
+games = scoreboard.ScoreBoard().get_json()
+print(games)
+
+# json
+# json = games.get_json()
+# print(json)
+
+# dictionary
+# games.get_dict()
+
+# teams = teams.get_teams()
+# print(teams)
+
+# play = playbyplay.PlayByPlay('0022200081')
+# print(play.get_json())
+
+# scores = boxscore.BoxScore('0012200001')
+# print(scores.get_json())
+
+# scores = boxscore.BoxScore('0022200755')
+# print(scores.get_json())
+
+# team = commonteamroster.CommonTeamRoster('1610612737')
+# print(team.get_dict())
+
+# schedule = leagueseasonmatchups.LeagueSeasonMatchups(league_id='00', season='2022-23',
+# season_type_playoffs='Regular Season', per_mode_simple='Totals')
+# print(schedule.get_json())
+
+# games = scoreboardv2.ScoreboardV2(game_date='2023-04-09')
+# df = games.get_normalized_json()
+# print(df)
+
+# game = scoreboard.Scoreboard(league_id='00', day_offset='0', game_date='2022-10-20')
+# print(game.get_normalized_json())
